9a1044203d33fa42b79b1f4268af7cea8dcc8e28
gcantu <39506132+cantugabriela@users.noreply.github.com>
2018-10-26 13:02:36 -0500

    Added city field, it filters with one or both values

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,58 @@
 // Variables
 var button = d3.select("#filter-btn");
-var inputField1 = d3.select("#datetime"); 
-// var inputField2 = d3.select("#city"); 
-var tbody = d3.select("tbody"); 
-var resetbtn = d3.select("#reset-btn"); 
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments" ]
+var inputField1 = d3.select("#datetime");
+var inputField2 = d3.select("#city");
+var tbody = d3.select("tbody");
+var resetbtn = d3.select("#reset-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-var populate = (dataInput, filterStatus)=>{
+var populate = (dataInput) => {
 
-      dataInput.forEach( ufo_sightings =>
-      {
-        var row = tbody.append("tr");
-        columns.forEach(column=> row.append("td").text(ufo_sightings[column])
-        )
-      });
-  }
+  dataInput.forEach(ufo_sightings => {
+    var row = tbody.append("tr");
+    columns.forEach(column => row.append("td").text(ufo_sightings[column])
+    )
+  });
+}
 
 //Populate table
 populate(data, false);
 
 // Filter by attribute
-button.on("click", ()=> {
+button.on("click", () => {
   d3.event.preventDefault();
   var inputDate = inputField1.property("value");
-  // var inputCity = inputField2.property("value");
-  // Filter by date matching input value
-  var filterData = data.filter(data => data.datetime === inputDate);
-  
+  var inputCity = inputField2.property("value");
+  // Filter by field matching input value
+  var filterDate = data.filter(data => data.datetime === inputDate);
+  console.log(filterDate)
+  var filterCity = data.filter(data => data.city === inputCity);
+  console.log(filterCity)
+  var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
+  console.log(filterData)
+
   // Add filtered sighting to table
   tbody.html("");
-  populate(filterData, true);
+
+let response ={
+  filterData, filterCity, filterDate
+}
+
+console.log(response); 
+  if(response){
+    if(response.filterData.length !== 0)
+    {
+      populate(filterData);
+    }
+    else {
+      populate(filterCity) || populate(filterDate);
+    }
+}
 })
 
 
-resetbtn.on("click", () =>{
+resetbtn.on("click", () => {
   tbody.html("");
-  populate(data, false); 
+  populate(data)
   console.log("Table reset")
 })
